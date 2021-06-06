9167631bbf4dbcd1e624190a6214632b7c6fd2a7
AdnanKuru <adnankuru.52@gmail.com>
2021-06-06 11:52:55 +0300

    created

---
+++ src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -0,0 +1,9 @@
+package kodlamaio.northwind;
+public class NorthwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindApplication.class, args);
+	}
+	
+
+}
